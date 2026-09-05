c880cdccac7a689728d1eb32d258dd350c0b6726
radheshyam123Q <72188819+radheshyam123Q@users.noreply.github.com>
2026-09-05 21:32:57 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0 , maxi = 0;
+        for(auto it : nums){
+            if(it){
+                count++ ;
+                maxi = max(count, maxi) ;
+            }
+            else count = 0 ;
+        }
+        return maxi ;
+    }
+};
